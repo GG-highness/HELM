c3f8b5477d56d85ee0c5d2d758e54cd6d4a4aec0
wisteria <63172882+GG-highness@users.noreply.github.com>
2026-04-08 14:35:06 +0900

    feat: #23 ruff & mypy 導入 (#25)

--- backend/app/models/card_name.py
+++ backend/app/models/card_name.py
@@ -1,19 +1,27 @@
+"""CardName model."""
+
 from sqlmodel import Field, SQLModel
 
 
 class CardNameBase(SQLModel):
-    name: str = Field(unique=True)
+    """CardNameBase."""
+
+    name: str = Field(unique=True, description="Card name.")
 
 
 class CardName(CardNameBase, table=True):
+    """CardName."""
+
     __tablename__ = "card_names"
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True, description="Primary key.")
 
 
 class CardNameCreate(CardNameBase):
-    pass
+    """CardNameCreate."""
 
 
 class CardNameRead(CardNameBase):
-    id: int
+    """CardNameRead."""
+
+    id: int = Field(description="Primary key.")
